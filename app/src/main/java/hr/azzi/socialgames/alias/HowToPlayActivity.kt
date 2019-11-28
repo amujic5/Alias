4b0662a639e7c648b28439e11166d364622205f5
Azzaro Mujic <azzaro.mujic@undabot.com>
2019-11-29 00:15:02 +0100

    Styles and new build.

--- app/src/main/java/hr/azzi/socialgames/alias/HowToPlayActivity.kt
+++ app/src/main/java/hr/azzi/socialgames/alias/HowToPlayActivity.kt
@@ -1,10 +1,47 @@
 package hr.azzi.socialgames.alias
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import kotlinx.android.synthetic.main.activity_how_to_play.*
+import java.io.File
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Size
+import android.graphics.Matrix
+import android.util.Log
+import android.view.Surface
+import android.view.TextureView
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.camera.core.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+import android.os.StrictMode
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.os.Handler
+import androidx.camera.core.impl.utils.executor.CameraXExecutors
+import java.util.concurrent.Executor
+
+
+// This is an arbitrary number we are using to keep track of the permission
+// request. Where an app has multiple context for requesting permission,
+// this can help differentiate the different contexts.
+private const val REQUEST_CODE_PERMISSIONS = 10
 
 class HowToPlayActivity : AppCompatActivity() {
+
+    private lateinit var videoCapture: VideoCapture
+    private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +53,134 @@ class HowToPlayActivity : AppCompatActivity() {
         gotItButton.setOnClickListener {
             this.finish()
         }
+
+
+//        viewFinder = findViewById(R.id.view_finder)
+//
+//        // Request camera permissions
+//        if (allPermissionsGranted()) {
+//            viewFinder.post { startCamera() }
+//        } else {
+//            ActivityCompat.requestPermissions(
+//                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+//        }
+//
+//        // Every time the provided texture view changes, recompute layout
+//        viewFinder.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+//            updateTransform()
+//        }
+//
+//        val builder = StrictMode.VmPolicy.Builder()
+//        StrictMode.setVmPolicy(builder.build())
     }
+
+    fun createInstagramIntent(type: String, uri: Uri){
+        val share = Intent(Intent.ACTION_SEND);
+
+        share.type = type
+        share.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        share.putExtra(Intent.EXTRA_STREAM, uri);
+        share.putExtra(
+            "android.intent.extra.TEXT",
+            Uri.parse(" https://play.google.com/store/apps/details?id=hr.azzi.socialgames.alias")
+        )
+
+        // Broadcast the Intent.
+        startActivity(Intent.createChooser(share, "Share to"));
+    }
+
+    // Add this after onCreate
+
+    private val executor = Executors.newSingleThreadExecutor()
+    private lateinit var viewFinder: TextureView
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(
+                    this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder().build()
+        val preview = Preview(previewConfig)
+
+        preview.setOnPreviewOutputUpdateListener {
+            viewFinder.surfaceTexture = it.surfaceTexture
+        }
+
+        val videoCaptureConfig = VideoCaptureConfig.Builder()
+            .apply {
+                setTargetRotation(viewFinder.display.rotation)
+            }
+            .build()
+        videoCapture = VideoCapture(videoCaptureConfig)
+
+        CameraX.bindToLifecycle(this, preview, videoCapture)
+
+
+        val file = File(externalMediaDirs.first(),
+            "${System.currentTimeMillis()}.mp4")
+
+        Handler().postDelayed({
+            videoCapture.startRecording(file, executor, object: VideoCapture.OnVideoSavedListener {
+                override fun onVideoSaved(file: File) {
+                    Log.i("tag", "Video File : $file")
+                    createInstagramIntent("video/*", Uri.fromFile(file))
+                }
+
+                override fun onError(
+                    videoCaptureError: VideoCapture.VideoCaptureError,
+                    message: String,
+                    cause: Throwable?
+                ) {
+                    Log.i("tag", "Video Error: $message")            }
+
+            })
+
+            Handler().postDelayed({
+                videoCapture.stopRecording()
+            }, 5000)
+        }, 5000)
+
+    }
+
+    private fun updateTransform() {
+        val matrix = Matrix()
+
+        // Compute the center of the view finder
+        val centerX = viewFinder.width / 2f
+        val centerY = viewFinder.height / 2f
+
+        // Correct preview output to account for display rotation
+        val rotationDegrees = when(viewFinder.display.rotation) {
+            Surface.ROTATION_0 -> 0
+            Surface.ROTATION_90 -> 90
+            Surface.ROTATION_180 -> 180
+            Surface.ROTATION_270 -> 270
+            else -> return
+        }
+        matrix.postRotate(-rotationDegrees.toFloat(), centerX, centerY)
+
+        // Finally, apply transformations to our TextureView
+        viewFinder.setTransform(matrix)
+    }
+
 }
