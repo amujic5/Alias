261aa0b37b3d435bdb8954f5fd772452389ae73a
Azzaro Mujic <azzaro.mujic@undabot.com>
2019-12-05 16:57:05 +0100

    Local words.

--- app/src/main/java/hr/azzi/socialgames/alias/Service/DictionaryService.kt
+++ app/src/main/java/hr/azzi/socialgames/alias/Service/DictionaryService.kt
@@ -1,26 +1,12 @@
 package hr.azzi.socialgames.alias.Service
 
-import android.content.ContentValues.TAG
-import android.util.Log
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import android.content.Context
 import hr.azzi.socialgames.alias.Models.Dictionary
+import hr.azzi.socialgames.alias.R
+import org.json.JSONObject
+import java.io.IOException
+import java.nio.charset.Charset
 
-class DatabaseUtil {
-    companion object {
-        private val firebaseDatabase: FirebaseDatabase = FirebaseDatabase.getInstance()
-
-        init {
-            firebaseDatabase.setPersistenceEnabled(true)
-        }
-
-        fun getDatabase() : FirebaseDatabase {
-            return firebaseDatabase
-        }
-    }
-}
 
 class DictionaryService {
 
@@ -29,91 +15,58 @@ class DictionaryService {
         var playingDictionary: Dictionary? = null
     }
 
-    private val childRef by lazy {
-        DatabaseUtil.getDatabase().getReference("dictionaries")
-    }
+    fun getDictionaries(context: Context):  MutableList<Dictionary> {
 
-    var dictionaries: MutableList<Dictionary> = mutableListOf()
+        var localDictionaries: MutableList<Dictionary> = mutableListOf()
 
-    init {
+        val jsonArray = JSONObject(loadJSONFromAsset(context)).getJSONArray("dictionaries")
 
-        childRef.child("0").child("")
+        for (i in 0 until jsonArray.length()) {
+            val item = jsonArray.getJSONObject(i)
 
-        childRef.addValueEventListener(object : ValueEventListener {
+            val imageUrlString = item.getString("imageURLString")
+            val language = item.getString("language")
+            val languageCode = item.getString("languageCode")
 
-            override fun onDataChange(p0: DataSnapshot) {
-                val objects = (p0.value as ArrayList<Any>)
+            var words: MutableList<String> = mutableListOf()
 
-                objects.forEach {
-                    if (it is Map<*, *>) {
-                        val json: Map<String, Any> = it as Map<String, Any>
-                        val dic = Dictionary.dictionaryFromJSON(json)
-                        val size = dic.words.size
-                        dictionaries.add(dic)
-                    }
-                }
-
+            val wordsJsonArray = item.getJSONArray("words")
+            for (i in 0 until wordsJsonArray.length()) {
+                val word = wordsJsonArray.getString(i)
+                words.add(word)
             }
 
-            override fun onCancelled(error: DatabaseError) {
-                Log.w(TAG, "Failed to read value.", error.toException())
-            }
-        })
-    }
-
-    fun fetchWords(language: String, callback: (ArrayList<String>) -> Unit) {
-
-        DatabaseUtil.getDatabase().getReference("words").child(language).addValueEventListener(object : ValueEventListener {
-
-            override fun onDataChange(p0: DataSnapshot) {
-                val value = p0.value
-                Log.w(TAG, "vvvv $value")
-                val words = (p0.value as ArrayList<String>)
-                callback(words)
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                val emptyWords: ArrayList<String> = ArrayList()
-                callback(emptyWords)
-            }
-        })
-    }
-
-    fun fetchDictionaries(callback: (MutableList<Dictionary>) -> Unit) {
-
-        if (!dictionaries.isEmpty()) {
-            callback(dictionaries)
-            return
-        } else {
-
-            childRef.addValueEventListener(object : ValueEventListener {
-
-                override fun onDataChange(p0: DataSnapshot) {
-                    var localDictionaries: MutableList<Dictionary> = mutableListOf()
-                    val objects = (p0.value as ArrayList<Any>)
-
-                    objects.forEach {
-                        if (it is Map<*, *>) {
-                            val json: Map<String, Any> = it as Map<String, Any>
-                            val dic = Dictionary.dictionaryFromJSON(json)
-
-                            Log.w(TAG, "lalala $dic.words.size")
-                            localDictionaries.add(dic)
-                        }
-                    }
-
-
-                    dictionaries = localDictionaries
-                    callback(dictionaries)
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    callback(dictionaries)
-                }
-            })
-
+            val dictionary = Dictionary(language, languageCode, words, imageUrlString)
+            localDictionaries.add(dictionary)
 
         }
+
+        return localDictionaries
+    }
+
+    fun loadJSONFromAsset(context: Context): String {
+        var json: String? = null
+        try {
+
+            val `is` = context.resources.openRawResource(R.raw.full_json)
+
+            val size = `is`.available()
+
+            val buffer = ByteArray(size)
+
+            `is`.read(buffer)
+
+            `is`.close()
+
+            json = String(buffer, Charset.forName("UTF-8"))
+
+
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            return ""
+        }
+
+        return json
     }
 
 }
